a65d0ea5adf82285759a72638af0456d2d46bb45
João Rodrigo <rodrigo@xsys.com.br>
2023-03-22 12:31:52 -0300

    feat: update B controller

--- GitTests/Controllers/FeatureBController.cs
+++ GitTests/Controllers/FeatureBController.cs
@@ -18,8 +18,8 @@ namespace GitTests.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetFeatureB")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpPost(Name = "PostFeatureB")]
+        public IEnumerable<WeatherForecast> Post()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
