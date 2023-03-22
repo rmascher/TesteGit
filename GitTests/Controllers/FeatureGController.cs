cb42877e03b2337b3b87de673fe941b8572ec25f
João Rodrigo <rodrigo@xsys.com.br>
2023-03-22 11:57:36 -0300

    feat: add G controller

---
+++ GitTests/Controllers/FeatureGController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitTests.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FeatureGController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<FeatureGController> _logger;
+
+        public FeatureGController(ILogger<FeatureGController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetFeatureG")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
